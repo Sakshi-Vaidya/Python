f09f8e2e8ca99fd11c6781f95cd59651d09384de
Sakshi-Vaidya <110716058+Sakshi-Vaidya@users.noreply.github.com>
2023-11-28 11:57:03 +0530

    Add files via upload

---
+++ ttospeech.py
@@ -0,0 +1,7 @@
+#step1: Install the pyttsx3 module
+#step 2: import the pyttsx3 module
+import pyttsx3
+
+engine=pyttsx3.init()
+engine.say("Hey Happy Birthday Dear May god bless you")
+engine.runAndWait()
