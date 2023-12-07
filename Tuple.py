39d03a70c63f08d7b9a576cc540a1c20844d31a5
Sakshi-Vaidya <110716058+Sakshi-Vaidya@users.noreply.github.com>
2023-12-07 11:48:05 +0530

    Create Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,10 @@
+my_tuple_1 = (11, 14, 0)
+
+print("The tuple is : ")
+print(my_tuple_1)
+N = 5
+
+my_result = ((my_tuple_1, ) * N)
+
+print("The tuple after duplicating "+ str(N) + " times is")
+print(my_result)
