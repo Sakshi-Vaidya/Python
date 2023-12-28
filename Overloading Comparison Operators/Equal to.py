e157f2de934d668e8343a236fe4dabcbc1ec5162
Sakshi-Vaidya <110716058+Sakshi-Vaidya@users.noreply.github.com>
2023-12-29 00:03:19 +0530

    Create Equal to.py

---
+++ Overloading Comparison Operators/Equal to.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # overload < operator
+    def __eq__(self, other):
+        return self.age == other.age
+
+p1 = Person("Alice", 20)
+p2 = Person("Bob", 30)
+
+print(p1 == p2) 
+print(p2 == p1) 
